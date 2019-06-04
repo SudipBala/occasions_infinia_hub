714b4dac9470c871611088551afe18d14506f522
t_barun <pujanthapa88.63@gmail.com>
2019-06-04 13:34:09 +0545

    Config for static and media urls

--- Occasions/settings.py
+++ Occasions/settings.py
@@ -125,3 +125,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+SHELL_PLUS = "ipython"
+
+FILE_UPLOAD_DIRECTORY_PERMISSIONS = 0o0777
+FILE_UPLOAD_PERMISSIONS = 0o0644
+
+STATICFILES_DIRS = []
