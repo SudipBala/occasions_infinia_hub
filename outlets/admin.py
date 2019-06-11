131e06ee6427ca6f0b33d2b25976f869a26e7b83
t_barun <pujanthapa88.63@gmail.com>
2019-06-11 13:03:05 +0545

    Added models in admin site

--- outlets/admin.py
+++ outlets/admin.py
@@ -3,12 +3,12 @@ from django.contrib import admin
 # Register your models here.
 
 from outlets.outlet_models import Outlet
-from outlets.stock_models import Category, Item
-
+from outlets.stock_models import Category, BaseItem, OutletItem, OutletStock
 
 # admin.site.register(Stock)
 admin.site.register(Category)
-admin.site.register(Item)
+admin.site.register(OutletItem)
+admin.site.register(OutletStock)
 admin.site.register(Outlet)
 
 
