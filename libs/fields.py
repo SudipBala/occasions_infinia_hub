ee31fa7f7dd954f5fd9c7ab5fb20eeb351bcc1c6
t_barun <pujanthapa88.63@gmail.com>
2019-06-11 13:02:09 +0545

    Custom JSON field

--- libs/fields.py
+++ libs/fields.py
@@ -1,5 +1,64 @@
+import json
+import yaml
 from django import forms
+from django.contrib.postgres.fields import JSONField
+from django.utils.translation import gettext_lazy as _
+
+from django.contrib.postgres.forms.jsonb import InvalidJSONInput
 from easy_thumbnails.fields import ThumbnailerImageField
+from yaml.parser import ParserError
+from yaml.scanner import ScannerError
+
+
+class JSONFormField(forms.CharField):
+    default_error_messages = {
+        'invalid': _("'%(value)s' value must be valid JSON."),
+    }
+
+    def __init__(self, **kwargs):
+        kwargs.setdefault('widget', forms.Textarea)
+        super(JSONFormField, self).__init__(**kwargs)
+
+    def to_python(self, value):
+        if self.disabled:
+            return value
+        if value in self.empty_values:
+            return None
+        try:
+            return yaml.safe_load(value)
+        except (ValueError, ScannerError, ParserError):
+            return json.loads(value)
+        except ValueError:
+            raise forms.ValidationError(
+                self.error_messages['invalid'],
+                code='invalid',
+                params={'value': value},
+            )
+        except:  # FIXME
+            raise  # uncomment me later
+
+    def bound_data(self, data, initial):
+        if self.disabled:
+            return initial
+        try:
+            return yaml.safe_load(data)
+        except (ValueError, ScannerError, ParserError):
+            return json.loads(data)
+        except ValueError:
+            return InvalidJSONInput(data)
+
+    def prepare_value(self, value):
+        if isinstance(value, InvalidJSONInput):
+            return value
+        # json.loads dumps to unicode, yaml dumps to whatever the original encoding was in
+        if isinstance(value, dict):
+            return str(value)
+        if value:
+            value = yaml.safe_load(value)
+        else:
+            return {}
+        if isinstance(value, dict):
+            return str(value)
 
 
 class SizeRestrictedThumbnailerField(ThumbnailerImageField):
@@ -33,3 +92,10 @@ class SizeRestrictedThumbnailerField(ThumbnailerImageField):
         except AttributeError:
             pass
         return data
+
+
+class CustomJsonField(JSONField):
+    def formfield(self, **kwargs):
+        defaults = {'form_class': JSONFormField}
+        defaults.update(kwargs)
+        return super(CustomJsonField, self).formfield(**defaults)
