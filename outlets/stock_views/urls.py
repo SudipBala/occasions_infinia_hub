dba0c9a2549a412e93d1e08af2123fe33a9596dd
t_barun <pujanthapa88.63@gmail.com>
2019-06-24 15:16:03 +0545

    URLS for outlet stock and item

--- outlets/stock_views/urls.py
+++ outlets/stock_views/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from outlets.stock_views.views import ListStock, DetailStock
+from outlets.stock_views.views import ListStock, DetailStock, AddStock, AddItem
 
 app_name = 'stocks'
 
 urlpatterns = [
     path('', ListStock.as_view(), name="list"),
     path('list/', ListStock.as_view(), name="list"),
-    # path('create/', CreateStock.as_view(), name="create"),
+    path('create/', AddStock.as_view(), name="create"),
+    path('add-item/', AddItem.as_view(), name="add_item"),
     path('<int:pk>/', DetailStock.as_view(), name="detail"),
 ]
