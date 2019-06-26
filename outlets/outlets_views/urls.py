820119f01b246248c71901bf857647fccd169ffb
Sudip <sudipbala01@gmail.com>
2019-06-26 13:50:21 +0545

    Items CRUD , outlet listing

--- outlets/outlets_views/urls.py
+++ outlets/outlets_views/urls.py
@@ -1,10 +1,17 @@
 
 from django.urls import path
 
-from outlets.outlets_views.views import OutletsCreateView
+from outlets.outlets_views.views import OutletsCreateView, ItemList, ItemDetail, ItemCreate, ItemUpdate, \
+    ItemDelete, outlet_list_view
 
 urlpatterns = [
     # path('create/', outlet_create_view, name="list_outlets")
 
-    path('list/', OutletsCreateView.as_view(), name="list_outlets")
+    path('create/', OutletsCreateView.as_view(), name="create_outlets"),
+    path('outlets/', outlet_list_view),
+    path('item/', ItemList.as_view(), name='item_list'),
+    path('item/<int:pk>', ItemDetail.as_view(), name='item_detail'),
+    path('create/', ItemCreate.as_view(), name='item_create'),
+    path('update/<int:pk>', ItemUpdate.as_view(), name='item_update'),
+    path('delete/<int:pk>', ItemDelete.as_view(), name='item_delete'),
 ]
--- outlets/outlets_views/views.py
+++ outlets/outlets_views/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 from outlets.forms import OutletForm
 from outlets.outlet_models import Outlet
+from outlets.stock_models import BaseItem
 
 
 class OutletsCreateView(View):
@@ -21,3 +23,43 @@ class OutletsCreateView(View):
         context = {'outlets': form}
         return render((request, 'outlets/create_outlet.html', context))
 
+
+def outlet_create_view(request):
+    form = OutletForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    context = {
+        'form': form
+    }
+    return render(request, "outlets/create_outlet.html", context)
+
+
+def outlet_list_view(request):
+    qs = Outlet.objects.all()
+    template_name = "outlets/list_outlets.html"
+    context = {'object_list': qs}
+    return render(request, template_name, context)
+
+
+class ItemList(ListView):
+    model = BaseItem
+    fields = '__all__'
+
+
+class ItemDetail(DetailView):
+    model = BaseItem
+
+
+class ItemCreate(CreateView):
+    model = BaseItem
+    fields = '__all__'
+
+
+class ItemUpdate(UpdateView):
+    model = BaseItem
+    fields = '__all__'
+
+
+class ItemDelete(DeleteView):
+    model = BaseItem
+    fields = '__all__'
