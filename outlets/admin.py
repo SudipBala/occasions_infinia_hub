71017fe56599d1cafd42140624aa753191a494b1
Sudip <sudipbala01@gmail.com>
2019-07-24 16:23:26 +0545

    Altered outlet lisit view in default django admin site

--- outlets/admin.py
+++ outlets/admin.py
@@ -1,13 +1,16 @@
-from outlets.outlet_models import Outlet
+from leaflet.admin import LeafletGeoAdmin
 from django.contrib import admin
+
+from outlets.outlet_models import Outlet
 from outlets.stock_models import Category, OutletItem, OutletStock
+
+
+class OutletAdmin(LeafletGeoAdmin):
+    list_display = ('display_name', 'location')
 
 
 # admin.site.register(Stock)
 admin.site.register(Category)
 admin.site.register(OutletItem)
 admin.site.register(OutletStock)
-admin.site.register(Outlet)
-
-
-
+admin.site.register(Outlet, OutletAdmin)
