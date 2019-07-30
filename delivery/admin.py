c1c02a5e6ca13c058c0b093cffbf2a8192da9af7
t_barun <pujanthapa88.63@gmail.com>
2019-07-30 15:07:02 +0545

    Registered tracking details to admin

--- delivery/admin.py
+++ delivery/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from delivery.models import DeliveryPolicy
+from delivery.models import DeliveryPolicy, TrackingDetails
 
 admin.site.register(DeliveryPolicy)
+admin.site.register(TrackingDetails)
