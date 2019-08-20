0938915e8ca9999c0d5912bfacf930fa3b2bf67b
Sudip <sudipbala01@gmail.com>
2019-08-20 13:19:05 +0545

    Item serializer

--- api/stocks_api/serializers.py
+++ api/stocks_api/serializers.py
@@ -6,7 +6,11 @@ from outlets.stock_models import OutletStock, BaseItem
 class ItemSerializer(ModelSerializer):
     class Meta:
         model = BaseItem
-        fields = "__all__"
+        fields = [
+            'display_name',
+            'unit',
+            'image'
+        ]
 
 
 class StocksListSerializer(ModelSerializer):
