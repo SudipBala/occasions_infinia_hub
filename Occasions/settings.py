6ae8b36e556eb8e5246475a65532f17e57ef8cce
Sudip <sudipbala01@gmail.com>
2019-09-02 13:59:50 +0545

    Added default authentication class as JTWAuthentication

--- Occasions/settings.py
+++ Occasions/settings.py
@@ -161,7 +161,10 @@ LOGIN_REDIRECT_URL = '/outlets/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5
+    'PAGE_SIZE': 5,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
 }
 
 LEAFLET_CONFIG = {
